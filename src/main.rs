93c5105f23cbbad07ac3d7b627da9e47c5e05366
Ryan Muther <muther22@gmail.com>
2025-01-02 20:26:42 -0500

    Removed print statements for debugging from main

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,10 @@ fn main() {
     let test_path = "src/test/brazil.jpg";
     println!{"Loading image from: {}", test_path};
     let img = IIIFImage::new(test_path);
-    println!("Created image with ID {}", img.id());
 
     let info = ImageInfo::from_image(&img);
-    println!("Created image info {}", info);
 
     let json = InfoJSON::new(&info, "test_image/".to_string(), "3.0".to_string());
-    println!("Created info json {}", json.id());
-    // println!("{}", json.to_json());
 
     let tiler = Tiler::new(&info,"3.0");
     tiler.generate_tiles("iiif");
