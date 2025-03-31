7655bb8d8915c298b42ff953bd9c1d3c8a8ac1e1
Ryan Muther <ryan_muther@fas.harvard.edu>
2025-03-31 17:21:59 -0400

    Removed manual default implementation for IIIFVersion

--- src/Info_Json.rs
+++ src/Info_Json.rs
@@ -1,27 +1,22 @@
 /**
  * This class generates the IIIF info.json for an image
  */
-use serde_json::{json, Value, Map};
+use serde_json::{json, Map, Value};
 
 use crate::Image_Info::ImageInfo;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Default)]
 pub enum IIIFVersion {
     VERSION3,
+    #[default]
     VERSION211,
- }
-
- impl Default for IIIFVersion {
-    fn default() -> Self {
-        IIIFVersion::VERSION211
-    }
- }
+}
 
 #[derive(Debug, PartialEq)]
 pub struct InfoJSON<'a> {
     image_info: &'a ImageInfo<'a>,
     uri: String,
-    version: &'a IIIFVersion
+    version: &'a IIIFVersion,
 }
 
 impl<'a> InfoJSON<'a> {
@@ -29,7 +24,7 @@ impl<'a> InfoJSON<'a> {
         InfoJSON {
             image_info,
             uri: uri.to_string(),
-            version: version
+            version,
         }
     }
 
@@ -50,22 +45,37 @@ impl<'a> InfoJSON<'a> {
 
         match self.version {
             IIIFVersion::VERSION3 => {
-                info_json.insert("@context".to_owned(), Value::String("http://iiif.io/api/image/3/context.json".to_owned()));
+                info_json.insert(
+                    "@context".to_owned(),
+                    Value::String("http://iiif.io/api/image/3/context.json".to_owned()),
+                );
                 info_json.insert("id".to_owned(), Value::String(self.id()));
-                info_json.insert("type".to_owned(), Value::String("ImageService3".to_string()));
-                info_json.insert("profile".to_owned(), Value::String("level0".to_string())); 
-            } 
+                info_json.insert(
+                    "type".to_owned(),
+                    Value::String("ImageService3".to_string()),
+                );
+                info_json.insert("profile".to_owned(), Value::String("level0".to_string()));
+            }
             IIIFVersion::VERSION211 => {
-                info_json.insert("@context".to_owned(), Value::String("http://iiif.io/api/image/2/context.json".to_owned()));
+                info_json.insert(
+                    "@context".to_owned(),
+                    Value::String("http://iiif.io/api/image/2/context.json".to_owned()),
+                );
                 info_json.insert("id".to_owned(), Value::String(self.id()));
-                info_json.insert("profile".to_owned(), Value::String("http://iiif.io/api/image/2/level0.json".to_owned()));
+                info_json.insert(
+                    "profile".to_owned(),
+                    Value::String("http://iiif.io/api/image/2/level0.json".to_owned()),
+                );
             }
         }
 
-        info_json.insert("protocol".to_owned(), Value::String("http://iiif.io/api/image".to_owned()));
+        info_json.insert(
+            "protocol".to_owned(),
+            Value::String("http://iiif.io/api/image".to_owned()),
+        );
         info_json.insert("width".to_owned(), Value::Number(self.width().into()));
         info_json.insert("height".to_owned(), Value::Number(self.height().into()));
-        
+
         // Add sizes
         let sizes_json: Vec<Value> = self
             .image_info
